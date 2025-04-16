119590ff8de4558ebb2d7367016b8e41ed5154cd
Benjamin Marchand <bmarchand@ippon.fr>
2025-04-16 15:52:08 +0200

    Initial commit

---
+++ src/main/java/com/bmarchand/pokemon_composite/PokemonCompositeApplication.java
@@ -0,0 +1,13 @@
+package com.bmarchand.pokemon_composite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCompositeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmarchand/pokemon_composite/PokemonCompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmarchand.pokemon_composite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCompositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
